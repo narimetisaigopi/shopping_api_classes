8159299abd47df0f2d9299d8e991b5e85cc1cb48
Sai Gopi Narimeti <narimetisaigopi@gmail.com>
2024-08-19 10:40:36 +0530

    added caetgories and products

--- lib/screens/products_screen.dart
+++ lib/screens/products_screen.dart
@@ -16,6 +16,8 @@ class ProductsScreen extends StatefulWidget {
 
 class _ProductsScreenState extends State<ProductsScreen> {
   List<ProductModel> productModelList = [];
+  List categoriesList = [];
+
   bool isLoading = false;
   getData() async {
     setState(() {
@@ -35,9 +37,34 @@ class _ProductsScreenState extends State<ProductsScreen> {
     });
   }
 
+  getCategiores() async {
+    http.Response response = await http
+        .get(Uri.parse("https://fakestoreapi.com/products/categories"));
+    categoriesList = jsonDecode(response.body);
+    log("categoriesList $categoriesList");
+    setState(() {});
+  }
+
+  getProductsByCategory(String category) async {
+    setState(() {
+      isLoading = true;
+    });
+    productModelList.clear();
+    http.Response response = await http
+        .get(Uri.parse("https://fakestoreapi.com/products/category/$category"));
+    var data = jsonDecode(response.body);
+    for (var item in data) {
+      productModelList.add(ProductModel.fromJson(item));
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   void initState() {
     getData();
+    getCategiores();
     super.initState();
   }
 
@@ -66,43 +93,72 @@ class _ProductsScreenState extends State<ProductsScreen> {
       //         ),
       //       );
       //     }),
-      body: isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, childAspectRatio: 1),
-              itemCount: productModelList.length,
-              itemBuilder: (context, index) {
-                ProductModel productModel = productModelList[index];
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (builder) => ProductDetails(
-                                  productModel: productModel,
-                                )));
-                  },
-                  child: Card(
-                    child: Column(
-                      children: [
-                        Image.network(
-                          productModel.image,
-                          height: 100,
-                          width: double.infinity,
-                          fit: BoxFit.fill,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 45,
+            child: ListView.builder(
+                itemCount: categoriesList.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      getProductsByCategory(categoriesList[index]);
+                    },
+                    child: Card(
+                        child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(categoriesList[index]),
+                    )),
+                  );
+                }),
+          ),
+          Expanded(
+            child: isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2, childAspectRatio: 1),
+                    itemCount: productModelList.length,
+                    itemBuilder: (context, index) {
+                      ProductModel productModel = productModelList[index];
+                      return InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (builder) => ProductDetails(
+                                        productModel: productModel,
+                                      )));
+                        },
+                        child: Card(
+                          child: Column(
+                            children: [
+                              Image.network(
+                                productModel.image,
+                                height: 100,
+                                width: double.infinity,
+                                fit: BoxFit.fill,
+                              ),
+                              // Text(productModel.id.toString()),
+                              Text(
+                                productModel.title,
+                                maxLines: 1,
+                              ),
+                              Text(
+                                productModel.category,
+                                maxLines: 1,
+                              ),
+                              Text("\$${productModel.price.toString()}"),
+                            ],
+                          ),
                         ),
-                        // Text(productModel.id.toString()),
-                        Text(
-                          productModel.title,
-                          maxLines: 1,
-                        ),
-                        Text("\$${productModel.price.toString()}"),
-                      ],
-                    ),
-                  ),
-                );
-              }),
+                      );
+                    }),
+          ),
+        ],
+      ),
     );
   }
 }
